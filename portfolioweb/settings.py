fd1a29e6c139199e7a228622053ebe92b77181d5
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-16 16:11:20 +0000

    added localhost to allowed host

--- portfolioweb/settings.py
+++ portfolioweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2mhsn^$le3g@fyi$q93&j2ymdb=k4nn4zc$94agjn28g31a+ox
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['portfolio-janghala.herokuapp.com']
+ALLOWED_HOSTS = ['portfolio-janghala.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition

03a90b024438767904a565c27e85ededee0318fd
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-16 00:44:46 +0000

    added ckeditor and rechtexteditor function

--- folio/models.py
+++ folio/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 import uuid
+from ckeditor.fields import RichTextField
 # Create your models here.
 
 
@@ -7,7 +8,7 @@ import uuid
 class Project(models.Model):
     title = models.CharField(max_length=200)
     thumbnail = models.ImageField(null=True)
-    description = models.TextField()
+    description = RichTextField()
     slug = models.SlugField(null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=False)

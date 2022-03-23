53fadab934057541c23f956bb6347220f576873d
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-03-23 14:41:50 +0000

    updated dns name

--- portfolioweb/settings.py
+++ portfolioweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-2mhsn^$le3g@fyi$q93&j2ymdb=k4nn4zc$94agjn28g31a+ox
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['www.janghala.xyz','portfolio-janghala.herokuapp.com', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['www.janghala.xyz', 'janghala.xyz', 'portfolio-janghala.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition

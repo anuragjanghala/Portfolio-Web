f434e6ea60886141bf4a85efc035e4e00df7078a
anuragjanghala <42205669+anuragjanghala@users.noreply.github.com>
2022-04-18 20:50:24 +0100

    P:: To create schema for API; NEW:: added pyyaml, uritemplate, openapi

--- portfolioweb/urls.py
+++ portfolioweb/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.schemas import get_schema_view
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -16,6 +17,11 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('ckeditor/', include('ckeditor_uploader.urls')),
+    path('schema', get_schema_view(
+        title="Portfolio Website",
+        description="API for Folio_Api App",
+        version="1.0.0"
+    ), name='openapi-schema'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
